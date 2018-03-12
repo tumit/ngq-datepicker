9667275034c320f8b5761a077d11a5d760816212
tumit <tumit@live.com>
2018-03-12 14:23:12 +0700

    refactor: remove custom datepicker of JQuery interface chore: remove alpha version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgqDatepickerModule } from './modules/ngq-datepicker/ngq-datepicker.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgqDatepickerModule } from '../../public_api';
 
 @NgModule({
   declarations: [AppComponent],
