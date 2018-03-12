c7cee4504ffd6aeb1f359ff47b26ad5365af0a78
tumit <tumit@live.com>
2018-03-12 17:36:23 +0700

    feat(datepicker-options): add @Input(opts) refactor: update example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControlName, FormControl } from '@angular/forms';
+import { NgqDatepickerComponent, th_TH, en_GB } from './modules/ngq-datepicker/ngq-datepicker.component';
 
 @Component({
   selector: 'ngq-root',
@@ -10,10 +11,21 @@ export class AppComponent implements OnInit {
   title = 'ngq';
   fg: FormGroup;
 
+  opts = th_TH;
+
   ngOnInit() {
     const myDate = new Date(1981, 4, 2);
     this.fg = new FormGroup({
-      birthDate: new FormControl(myDate)
+      dateFrom: new FormControl(myDate),
+      dateTo: new FormControl(myDate)
     });
+  }
+
+  changeLang() {
+    if (this.opts.language === 'th-TH') {
+      this.opts = en_GB;
+    } else {
+      this.opts = th_TH;
+    }
   }
 }
--- src/app/modules/ngq-datepicker/ngq-datepicker.component.ts
+++ src/app/modules/ngq-datepicker/ngq-datepicker.component.ts
@@ -2,8 +2,9 @@ import { AfterViewInit, Component, forwardRef, HostBinding, HostListener, Input,
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import 'bootstrap-datepicker';
 
-const BASE_OPTION = { autoclose: true, format: 'dd/mm/yyyy' };
-const THAI_OPTION = { ...BASE_OPTION, language: 'th-TH' };
+export const BASE_OPTION = { autoclose: true, format: 'dd/mm/yyyy' } as DatepickerOptions;
+export const th_TH = { ...BASE_OPTION, language: 'th-TH' } as DatepickerOptions;
+export const en_GB = { ...BASE_OPTION, language: 'th-GB' } as DatepickerOptions;
 
 const NGQ_DATETIME_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
@@ -28,20 +29,43 @@ export class NgqDatepickerComponent implements ControlValueAccessor, AfterViewIn
   _datepicker: JQuery;
   _date: Date;
   _isDisabled: boolean;
+  _datePickerOptions: DatepickerOptions;
 
   constructor() {}
 
   propagateChange = _ => {};
   @HostListener('blur') onTouched = () => {};
 
+  @Input('opts')
+  set datePickerOptions(opts: DatepickerOptions) {
+    this._datePickerOptions = opts;
+    if (this.input) {
+      // destroy old datepicker
+      jQuery(this.input.nativeElement).datepicker('destroy');
+      // change value in text box follow option.language
+      const newVal = new Intl.DateTimeFormat(this._datePickerOptions.language).format(this._date);
+      jQuery(this.input.nativeElement).val(newVal);
+      // init datepicker
+      this.initDatepicker();
+    }
+  }
+
+  private initDatepicker() {
+    if (this.input) {
+      this._datepicker = jQuery(this.input.nativeElement).datepicker(this._datePickerOptions);
+
+      this._datepicker.datepicker().on('changeDate', (e: any) => {
+        this._date = e.date;
+        this.propagateChange(this._date);
+      });
+
+      this._datepicker.datepicker('update', this._date);
+      this._datepicker.prop('disabled', this._isDisabled);
+    }
+  }
+
   ngAfterViewInit() {
-    this._datepicker = jQuery(this.input.nativeElement).datepicker(THAI_OPTION);
-    this._datepicker.datepicker().on('changeDate', (e: any) => {
-      this._date = e.date;
-      this.propagateChange(this._date);
-    });
-    this._datepicker.datepicker('update', this._date);
-    this._datepicker.prop('disabled', this._isDisabled);
+    this.initDatepicker();
   }
 
   writeValue(obj: any): void {
