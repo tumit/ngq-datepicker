d9c5295e004a030c0e8001d10ca6d986840fec4d
tumit <tumit@live.com>
2018-03-23 14:37:25 +0700

    feat(thai-calendar): use year-offset calendar fix: when enter wrong date format then form control should be default value in input chore: add ci

--- src/app/modules/ngq-datepicker/ngq-datepicker.component.ts
+++ src/app/modules/ngq-datepicker/ngq-datepicker.component.ts
@@ -11,7 +11,11 @@ import {
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import 'bootstrap-datepicker';
 
-export const BASE_OPTION = { autoclose: true, format: 'dd/mm/yyyy', language: 'en-GB' } as DatepickerOptions;
+export const BASE_OPTION = {
+  autoclose: true,
+  format: 'dd/mm/yyyy',
+  language: 'en-GB'
+} as DatepickerOptions;
 export const th_TH = { ...BASE_OPTION, language: 'th-TH' } as DatepickerOptions;
 export const en_GB = { ...BASE_OPTION, language: 'en-GB' } as DatepickerOptions;
 
@@ -27,7 +31,8 @@ const NGQ_DATETIME_VALUE_ACCESSOR: any = {
   styleUrls: ['./ngq-datepicker.component.css'],
   providers: [NGQ_DATETIME_VALUE_ACCESSOR]
 })
-export class NgqDatepickerComponent implements ControlValueAccessor, AfterViewInit {
+export class NgqDatepickerComponent
+  implements ControlValueAccessor, AfterViewInit {
   @Input() hideIcon: false;
   @Input() id: string;
   @Input() class: string;
@@ -35,65 +40,67 @@ export class NgqDatepickerComponent implements ControlValueAccessor, AfterViewIn
 
   @ViewChild('input') input;
 
-  _datepicker: JQuery;
-  _date: Date;
+  _jQueryElement: JQuery;
+  _value: number;
   _isDisabled: boolean;
-  _datePickerOptions: DatepickerOptions;
+  _opts: DatepickerOptions;
 
   constructor() {}
 
-  propagateChange = _ => {};
-  @HostListener('blur') onTouched = () => {};
-
   // should re-initial datepicker when assign new options
   @Input('opts')
-  set datePickerOptions(opts: DatepickerOptions) {
-    this._datePickerOptions = opts;
-    if (this.input) {
-      // destroy old datepicker
-      jQuery(this.input.nativeElement).datepicker('destroy');
-      // change value in text box follow option.language
-      const newVal = !!this._date ? new Intl.DateTimeFormat(this._datePickerOptions.language).format(this._date) : '';
-      jQuery(this.input.nativeElement).val(newVal);
-      // init datepicker
+  set opts(opts: DatepickerOptions) {
+    this._opts = opts;
+    if (this._jQueryElement) {
+      this._jQueryElement.datepicker('destroy');
+      const newVal = !!this._value ? new Intl.DateTimeFormat(this._opts.language).format(this._value) : '';
+      this._jQueryElement.val(newVal);
       this.initDatepicker();
     }
   }
 
   ngAfterViewInit() {
+    this._jQueryElement = jQuery(this.input.nativeElement);
     this.initDatepicker();
+
   }
 
   private initDatepicker() {
-    if (this.input) {
-      this._datepicker = jQuery(this.input.nativeElement).datepicker(this._datePickerOptions);
-      this._datepicker.datepicker().on('changeDate', (e: any) => {
-        this._date = e.date;
-        this.propagateChange(this._date);
-      });
-      // should update when set value from writeValue()
-      this._datepicker.datepicker('update', this._date);
-      this._datepicker.prop('disabled', this._isDisabled);
-    }
+    this._opts = (this._opts) ? this._opts : th_TH;
+    this._jQueryElement.datepicker(this._opts);
+    this._jQueryElement.datepicker().on('changeDate', (e: any) => {
+      this._value = e.date;
+      this.propagateChange(this._value);
+    });
+
+    this._jQueryElement.datepicker('update', this._value);
+    this._jQueryElement.prop('disabled', this._isDisabled);
   }
 
   onChange(value: string) {
     if (!value) {
-      this._date = null;
-      this.propagateChange(this._date);
+      this._value = null;
+      this.propagateChange(this._value);
     } else {
       jQuery(this.input.nativeElement).datepicker('destroy');
       this.initDatepicker();
     }
   }
 
+  propagateChange = _ => {};
+
+  @HostListener('blur') onTouched = () => {};
+
   writeValue(obj: any): void {
-    this._date = obj;
+    this._value = obj;
   }
+
   registerOnChange(fn: any): void {
     this.propagateChange = fn;
   }
+
   registerOnTouched(fn: any): void {}
+
   setDisabledState?(isDisabled: boolean): void {
     this._isDisabled = isDisabled;
   }
